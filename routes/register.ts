12a36b4aed90a6518f7e949060cbde8a3fea6f5b
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-08-29 20:25:07 +0545

    Added Zod and Prisma error handling

--- routes/register.ts
+++ routes/register.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {z} from 'zod';
+import {z, ZodError} from 'zod';
 import bcrypt from 'bcrypt';
 import {PrismaClient} from '@prisma/client';
 
@@ -14,22 +14,41 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 const registerRoute = router.post('/',async (req, res)=>{
-    const data = req.body;
-    const parsedData = await user.parseAsync(data);
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(parsedData.password, salt);
-    const validData = {
-        name:parsedData.name,
-        email:parsedData.email,
-        password:password
-    };
-
-    const result = await prisma.user.create({
-        data:validData
-    });
-
-    res.status(201).json(result);
-
+    try{
+        const data = req.body;
+        const parsedData = await user.parseAsync(data);
+        const salt = await bcrypt.genSalt(10);
+        const password = await bcrypt.hash(parsedData.password, salt);
+        const validData = {
+            name:parsedData.name,
+            email:parsedData.email,
+            password:password
+        };
+    
+        const result = await prisma.user.create({
+            data:validData
+        });
+        res.status(201).json(result);
+    }
+    catch (err){
+        if(err instanceof ZodError){
+            res.status(400).json({"success":false,"error":"invalid input!"});
+        }
+        else if(err.code=="P2002" && err.meta.modelName == "User"){
+            if(err.meta.target[0] == "name"){
+                console.log(err);
+                res.status(400).json({"success":false,"error":"name already taken. Try something else!"});
+            }
+            else if(err.meta.target[0] == "email"){
+                console.log(err);
+                res.status(400).json({"success":false,"error":"email already in use!"});
+            }
+        }
+        else{
+            console.log(err);
+            res.status(500).json({"success":false,"error":"server error!"});
+        }
+    }
 });
 
 export default registerRoute;
