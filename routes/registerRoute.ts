e4977881aaeed03f6ce7f63352bbdbcb3d76a784
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-09-05 18:37:42 +0545

    added proper input validation for user registration

--- routes/registerRoute.ts
+++ routes/registerRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import register from '../controllers/register'
 import validate from '../middlewares/validate'
+import {registrationSchema} from '../zodSchemas/schema'
 
 const router = express.Router();
 
-const registerRoute = router.post('/',validate, register);
+const registerRoute = router.post('/',validate(registrationSchema), register);
 
 export default registerRoute;
