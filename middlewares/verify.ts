0b4e855755b42049e62f294f39054f6314b9e092
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-09-20 15:51:07 +0545

    separate access and refresh token verification functions

--- middlewares/verify.ts
+++ middlewares/verify.ts
@@ -1,50 +1,80 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import {PrismaClient} from '@prisma/client';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const prisma = new PrismaClient();
 
-export const verifyToken = async (req, res, next)=>{
+export const verifyAccessToken =
+  (secretKey: string) => async (req, res, next) => {
+    try {
+      //access the accessToken from cookies
+      const accessToken = req.cookies.SessionID;
+
+      //verify token signature
+      jwt.verify(accessToken, secretKey, async function (err, decoded) {
+        if (err) {
+          console.log(err);
+          res.status(401).json({ success: false, error: "Invalid token" });
+        }
+        const { id, type } = decoded;
+
+        //verify tokenType
+        if (type === "ACCESS") {
+          req.id = id;
+          next();
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ success: false, error: "server error!" });
+    }
+  };
+
+export const verifyRefreshToken = (secretKey:string) =>async (req, res, next) =>{
     try{
-        const cookie = req.headers["cookie"];
-        const token = cookie.split("=")[1]; //get actual access token from req.header
-        jwt.verify(token, process.env.TOKEN_SECRET, async function(err, decoded){
-            if(err){
-                console.log(err);
-                res.status(401).json({"success":false, "error":"Invalid token"});
+        const refreshToken = req.cookies.RefreshToken;
+        jwt.verify(refreshToken, secretKey, (err, decoded)=>{
+            if (err) {
+              console.log(err);
+              res.status(401).json({ success: false, error: "Invalid token" });
             }
-            const {id} = decoded;
-            req.user = await prisma.user.findUnique({
-                select:{
-                    id:true,
-                    role:true
-                },
-                where:{
-                        id:id
-                }
-            });
-            next();
+            const { id, tokenType } = decoded;            
+    
+            //verify tokenType
+            if (tokenType === "REFRESH") {
+              req.id = id;
+              next();
+            }
         });
-    }   
-    catch (err){
-        console.log(err);
-        res.status(500).json({"success":false, "error":"server error!"});
+
+    }
+    catch (err) {
+      console.log(err);
+      res.status(500).json({ success: false, error: "server error!" });
     }
 }
 
-export const verifyRole = async (req, res, next)=>{
-    try{
-        const role = req.user.role;
-        if(role==="admin"){
-            next();
-        }
-        else{
-            res.status(403).json({"success":false,"error":"access denied!"});
-        }
+export const verifyRole = async (req, res, next) => {
+  try {
+    console.log(req.id);
+    const userRole = await prisma.user.findUnique({
+      select: {
+        role: true,
+      },
+      where: {
+        id: req.id,
+      },
+    });
+    if (userRole["role"] === "admin") {
+      next();
+    } 
+    else {
+      res.status(403).json({ success: false, error: "access denied!" });
     }
-    catch (err){
-        console.log(err);
-        res.status(500).json({"success":false,"error":"server error!"});
-    }
-}
+  } 
+  catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "server error!" });
+  }
+};
