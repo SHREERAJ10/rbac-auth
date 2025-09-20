f47671ec9913e09ebc437d4bb3866365bba5c125
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-09-20 15:52:23 +0545

    add verifyAccessToken middleware

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import {verifyToken, verifyRole} from '../middlewares/verify';
+import dotenv from 'dotenv';
+import {verifyAccessToken, verifyRole} from '../middlewares/verify';
 const router = express.Router();
 
+dotenv.config();
 
-const adminRoute = router.get('/', verifyToken, verifyRole, (req, res)=>{
+const adminRoute = router.get('/', verifyAccessToken(process.env.TOKEN_SECRET), verifyRole, (req, res)=>{
     res.status(200).json({"success":true,"message":"welcome, admin!"});
 });
 
