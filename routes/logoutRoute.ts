76bd2dbdfbc471343c9310c01c58c14510f449fe
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-09-25 10:00:58 +0545

    create logoutRoute

---
+++ routes/logoutRoute.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { verifyRefreshToken } from '../middlewares/verify';
+import dotenv from 'dotenv';
+import logout from '../controllers/logout';
+import { resetTokens } from '../controllers/issueTokens';
+
+dotenv.config();
+
+const router = express.Router();
+
+const logoutRoute = router.post('/', verifyRefreshToken(process.env.REFRESH_TOKEN_SECRET), logout, resetTokens);
+
+export default logoutRoute; 
