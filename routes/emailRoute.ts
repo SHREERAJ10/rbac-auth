727ea0b6264dd6b61ecc10cf3f6ad79d61dbe35f
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-10-12 09:47:02 +0545

    add middlewares and functions for email verification

---
+++ routes/emailRoute.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { verifyAccessToken } from '../middlewares/verify';
+import dotenv from 'dotenv';
+import emailVerificationStuff from '../controllers/emailVerificationStuff';
+import sendEmail from '../controllers/sendEmail';
+import verifyEmailToken from '../controllers/verifyEmailToken';
+import checkEmailAlreadyVerified from '../controllers/checkEmailAlreadyVerified';
+
+dotenv.config();
+
+const router = express.Router();
+
+const emailRoute = router.post('/', verifyAccessToken(process.env.TOKEN_SECRET), checkEmailAlreadyVerified, emailVerificationStuff, sendEmail);
+
+router.get('/verifyToken', verifyAccessToken(process.env.TOKEN_SECRET), verifyEmailToken);
+
+
+export default emailRoute;
