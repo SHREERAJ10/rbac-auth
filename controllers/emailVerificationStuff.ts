a0864ec1216004fff2810a20445bd974d204ad11
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-10-12 09:44:33 +0545

    chore: add middleware to generate and store hashed verification token

--- controllers/emailVerificationStuff.ts
+++ controllers/emailVerificationStuff.ts
@@ -3,23 +3,54 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const emailVerification = async (req, res)=>{
+const emailVerification = async (req, res, next)=>{
 
-    const tokenExpiresInHours = 3;
-    const userId = req.id;
-    //generate token
-    const verificationToken = crypto.randomBytes(64).toString('hex');
+    try{
+        const tokenExpiresInHours = 3;
     
-    const tokenHash = crypto.createHash('sha256').update(verificationToken).digest('hex');
-
-    await prisma.emailVerification.create({
-        data:{
-            userId: userId,
-            verification_token_hash: tokenHash,
-            token_created_at: new Date(),
-            token_expires_at: new Date(Date.now() + tokenExpiresInHours * 60 *60 * 1000), //expiry time in ms
+        //generate token
+        const verificationToken = crypto.randomBytes(64).toString('hex');
+        
+        //hash the token
+        const tokenHash = crypto.createHash('sha256').update(verificationToken).digest('hex');
+    
+        const userId = req.id;
+        const user = await prisma.emailVerification.findFirst({
+            where:{
+                userId:userId
+            }
+        })
+    
+        if(user==null){
+            await prisma.emailVerification.create({
+                data:{
+                    userId: userId,
+                    verification_token_hash: tokenHash,
+                    token_created_at: new Date(),
+                    token_expires_at: new Date(Date.now() + tokenExpiresInHours * 60 *60 * 1000), //expiry time in ms
+                }
+            });
         }
-    });
+        else{
+            await prisma.emailVerification.update({
+                data:{
+                    verification_token_hash:tokenHash,
+                    token_created_at: new Date(),
+                    token_expires_at: new Date(Date.now() + tokenExpiresInHours * 60 * 60 * 1000),
+                },
+                where:{
+                    userId:userId
+                }
+            });
+        }
+        req.verificationToken = verificationToken;
+        next();
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).json({'success':false,'error':'unexpected server error!'});
+    }
+
 }
 
 export default emailVerification;
