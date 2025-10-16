9e5cc6b0bce0c368925e4c0c0c25900eb41668b3
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-10-16 21:10:43 +0545

    function generates and stores email verification token

---
+++ controllers/EmailVerification/verifyEmail.ts
@@ -0,0 +1,56 @@
+import crypto from 'crypto';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const verifyEmail = async (req, res, next)=>{
+
+    try{
+        const tokenExpiresInHours = 3;
+    
+        //generate token
+        const verificationToken = crypto.randomBytes(64).toString('hex');
+        
+        //hash the token
+        const tokenHash = crypto.createHash('sha256').update(verificationToken).digest('hex');
+    
+        const userId = req.id;
+        const user = await prisma.emailVerification.findFirst({
+            where:{
+                userId:userId
+            }
+        })
+    
+        if(user==null){
+            await prisma.emailVerification.create({
+                data:{
+                    userId: userId,
+                    verification_token_hash: tokenHash,
+                    token_created_at: new Date(),
+                    token_expires_at: new Date(Date.now() + tokenExpiresInHours * 60 *60 * 1000), //expiry time in ms
+                }
+            });
+        }
+        else{
+            await prisma.emailVerification.update({
+                data:{
+                    verification_token_hash:tokenHash,
+                    token_created_at: new Date(),
+                    token_expires_at: new Date(Date.now() + tokenExpiresInHours * 60 * 60 * 1000),
+                },
+                where:{
+                    userId:userId
+                }
+            });
+        }
+        req.verificationToken = verificationToken;
+        next();
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).json({'success':false,'error':'unexpected server error!'});
+    }
+
+}
+
+export default verifyEmail;
