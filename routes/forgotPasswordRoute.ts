0c7d9b9ee2e31b8a874a006f1e924f9a668afacd
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-10-16 21:15:57 +0545

    update helper functions name

--- routes/forgotPasswordRoute.ts
+++ routes/forgotPasswordRoute.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-import getUserIdFromEmail from '../controllers/PasswordReset/getUserIdFromEmail';
-import genAndStoreToken from '../controllers/PasswordReset/genAndStoreToken';
-import sendResetLink from '../controllers/PasswordReset/sendResetLink';
-import validateToken from '../controllers/PasswordReset/validateToken';
-import resetPassword from '../controllers/PasswordReset/resetPassword';
+import getUserIdFromEmail from '../utils/ForgotPassword/getUserIdFromEmail';
+import genAndStoreToken from '../utils/ForgotPassword/genAndStoreToken';
+import sendResetLink from '../utils/ForgotPassword/sendResetLink';
+import validatePasswordResetToken from '../middlewares/validatePasswordResetToken';
+import resetPassword from '../controllers/ForgotPassword/resetPassword';
 import validateSchema from '../middlewares/validateSchema';
 import { emailSchema, passwordSchema } from '../zodSchemas/schema';
-import invalidateResetToken from '../controllers/PasswordReset/invalidateResetToken';
+import invalidateResetToken from '../utils/ForgotPassword/invalidateResetToken';
 
 const forgotPasswordRoute = express.Router();
 
 forgotPasswordRoute.post('/', validateSchema(emailSchema), getUserIdFromEmail, genAndStoreToken, sendResetLink);
 
-forgotPasswordRoute.post('/token', validateSchema(passwordSchema), validateToken, resetPassword, invalidateResetToken);
+forgotPasswordRoute.post('/token', validateSchema(passwordSchema), validatePasswordResetToken, resetPassword, invalidateResetToken);
 
 export default forgotPasswordRoute;
